e055716eead888d6e928df565d73891994744496
Jeeshan <jeeshan.ahmed@soprasteria.com>
2021-08-10 22:28:30 +0530

    advance react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import UseStateObject from "./tutorial/useStateObject";
+import UseStateBasic from ".tutorial/UseStateBasic";
+
+function App() {
+  
+  return (<div className = 'container'>
+     <h2> Advance React js</h2>
+     <UseStateBasic/>
+     </div>
+    );
+}
+
+export default App;
